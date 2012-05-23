5f3bf5862b0a54df80aaefb77a52c053a23b24ad
Daisuke Miura <mhden@drakontia.com>
2012-05-23 14:47:03 +0900

    Change Incorrect Path

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 begin
-  require File.dirname(__FILE__) + '/../../../../spec/spec_helper'
+  require File.dirname(__FILE__) + '/../spec/spec_helper'
 rescue LoadError
   puts "You need to install rspec in your base app"
   exit
