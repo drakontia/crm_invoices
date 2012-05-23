ac6b5d56a061093503fc74878d9fbe3e281f453b
Daisuke Miura <mhden@drakontia.com>
2012-05-23 18:48:16 +0900

    Test in FatFreeCRM

--- lib/crm_invoices.rb
+++ lib/crm_invoices.rb
@@ -4,12 +4,12 @@ Rails.configuration.to_prepare do
   Account.send(:include, AccountInvoiceAssociations)
 
   # Make invoices observable.
-  ActivityObserver.instance.send :add_observer!, Invoice
-
+  #ActivityObserver.instance.send :add_observer!, Invoice
+  Rails.configuration.active_record.observers = []
   # Add :invoices plugin helpers.
   ActionView::Base.send(:include, InvoicesHelper)
 
 end
 
 # Make the invoices commentable.
-CommentsController.commentables = CommentsController.commentables + %w(invoice_id)
+#CommentsController.commentable = CommentsController.create %w(invoice_id)
